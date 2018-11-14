4b82bd38073950d8a70d5e8a2bb6d60fa5c4eead
Aring8510 <nakamu8510@gmail.com>
2018-11-14 18:39:40 +0900

    っッーを消す

--- jtor.py
+++ jtor.py
@@ -1,17 +1,22 @@
 from pykakasi import kakasi
+import re
 
 def conv_to_roman(text):
     global kakasi
     kakasi = kakasi()  # Generate kakasi instance
+    kakasi.setMode("J", "H")  # Japanese(kanji) to ascii
+    conv = kakasi.getConverter()
+    text = conv.do(text)
+
+    text = re.sub(r'[っッー]+',"",text)
 
     kakasi.setMode("H", "a")  # Hiragana to ascii
     kakasi.setMode("K", "a")  # Katakana to ascii
-    kakasi.setMode("J", "a")  # Japanese(kanji) to ascii
 
     kakasi.setMode("r", "Hepburn")  # Use Hepburn romanization
 
     conv = kakasi.getConverter()
-    result = conv.do(text)
-    return result
+    text = conv.do(text)
+    return text
 if __name__ == '__main__':
-    print(conv_to_roman("これはひらがなコレハカタカナ漢字"))
+    print(conv_to_roman("楽器"))

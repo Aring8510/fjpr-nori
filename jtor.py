efbe672cacfeb768ab87ad7f2578d09026206bfb
equal-l2 <eng.equall2@gmail.com>
2018-11-18 20:07:59 +0900

    Use own converter to convert hiragana to roman
    ひらがなからローマ字への変換は自前で行うことにした。
    変わった点は以下の通り
    - 「ウィ」などのpykakasiがうまく扱えないものに対応
    - FJPR変換に必要ない音はあらかじめ落とすようにした。
      これによりローマ字からFJPR変換する際の前処理が不要になった。
    
    Close #6

--- jtor.py
+++ jtor.py
@@ -5,18 +5,64 @@ def conv_to_roman(text):
     global kakasiObj
     kakasiObj = kakasi()  # Get kakasi instance
 
-    # ひらがなとカタカナで変換結果が違うので一旦ひらがなにする
+    # 形態素解析した上でひらがなに変換
     kakasiObj.setMode("H", "H")  # Hiragana to ascii
     kakasiObj.setMode("K", "H")  # Katakana to ascii
     kakasiObj.setMode("J", "H")  # Japanese(kanji) to ascii
-    convHiragana = kakasiObj.getConverter()
-    resultHiragana = convHiragana.do(text)
+    conv = kakasiObj.getConverter()
+    result = conv.do(text)
 
-    kakasiObj.setMode("H", "a")  # Hiragana to ascii
-    kakasiObj.setMode("K", "a")  # Katakana to ascii
-    kakasiObj.setMode("J", "a")  # Japanese(kanji) to ascii
-    kakasiObj.setMode("r", "Hepburn")  # Use Hepburn romanization
+    # ひらがなをローマ字に変換する
+    # FJPR変換に使えれば十分なのでいらない音は消す
 
-    convRoman = kakasiObj.getConverter()
-    result = convRoman.do(resultHiragana)
+    # 1文字の音
+    # 「お」・「を」  :どちらも「エイ」
+    # 「ん」は区切っておかないとナ行と間違われる
+    # 「ー」、「っ」はいずれ消えるのでここで消してしまう
+    d = {
+        "あ": "a",  "い": "i",  "う": "u",   "え": "e",  "お": "o",
+        "ぁ": "a",  "ぃ": "i",  "ぅ": "u",   "ぇ": "e",  "ぉ": "o",
+        "か": "ka", "き": "ki", "く": "ku",  "け": "ke", "こ": "ko",
+        "さ": "sa", "し": "si", "す": "su",  "せ": "se", "そ": "so",
+        "た": "ta", "ち": "ti", "つ": "tu",  "て": "te", "と": "to",
+        "な": "na", "に": "ni", "ぬ": "nu",  "ね": "ne", "の": "no",
+        "は": "ha", "ひ": "hi", "ふ": "hu",  "へ": "he", "ほ": "ho",
+        "ま": "ma", "み": "mi", "む": "mu",  "め": "me", "も": "mo",
+        "や": "ya",             "ゆ": "yu",              "よ": "yo",
+        "ら": "ra", "り": "ri", "る": "ru",  "れ": "re", "ろ": "ro",
+        "わ": "wa", "を": "o",  "ん": "'n'", "ー": None, "っ": None,
+        "が": "ga", "ぎ": "gi", "ぐ": "gu",  "げ": "ge", "ご": "go",
+        "ざ": "za", "じ": "zi", "ず": "zu",  "ぜ": "ze", "ぞ": "zo",
+        "だ": "da", "ぢ": "di", "づ": "du",  "で": "de", "ど": "do",
+        "ば": "ba", "び": "bi", "ぶ": "bu",  "べ": "be", "ぼ": "bo",
+        "ぱ": "pa", "ぴ": "pi", "ぷ": "pu",  "ぺ": "pe", "ぽ": "po",
+    }
+
+    # 2文字の音
+    d2 = {
+        # 特有の音
+        "いぇ": "ye", "うぃ": "wi", "うぇ": "we", "うぉ": "wo",
+        "じゃ": "ja", "じゅ": "ju", "じぇ": "je", "じょ": "jo",
+        # FJPR変換で消える音は1文字音相当へ変換
+        "きゃ": "ka", "きゅ": "ku", "きぇ": "ke", "きょ": "ko",
+        "しゃ": "sa", "しゅ": "su", "しぇ": "se", "しょ": "so",
+        "ちゃ": "ta", "ちゅ": "tu", "ちぇ": "te", "ちょ": "to",
+        "にゃ": "na", "にゅ": "nu", "にぇ": "ne", "にょ": "no",
+        "ひゃ": "ha", "ひゅ": "hu", "ひょ": "ho",
+        "みゃ": "ma", "みゅ": "mu", "みぇ": "me", "みょ": "mo",
+        "りゃ": "ra", "りゅ": "ru", "りぇ": "re", "りょ": "ro",
+        "ぎゃ": "ga", "ぎゅ": "gu", "ぎぇ": "ge", "ぎょ": "go",
+        "びゃ": "ba", "びゅ": "bu", "びょ": "bo",
+        "ぴゃ": "pa", "ぴゅ": "pu", "ぴょ": "po",
+        "ふぁ": "ha", "ふぃ": "hi", "ふぇ": "he",
+        "ゔぁ": "ba", "ゔぃ": "bi", "ゔぇ": "be", "ゔぉ": "bo",
+        "てぃ": "ti", "でぃ": "di", "でゅ": "du",
+    }
+
+    # 2文字の音を先に変換する
+    for k, v in d2.items():
+        result = result.replace(k, v)
+
+    result = result.translate(str.maketrans(d))
+
     return result
--- rtof.py
+++ rtof.py
@@ -2,40 +2,29 @@ import re
 
 
 def conv_to_fjpr(s):
-    print("原文 " + s)
+    print("RTOF 原文 " + s)
     vowels = "[aiueo]"  # 母音
     cons = "[kstnhmyrwgzdbpvjf]"  # 子音
     unit = f"{cons}?ei"  # 変換後のひとまとまり (ei または 子音 + ei)
-
-    s = re.sub("-", "", s)   # 長音を消す
-    s = re.sub("jie", "je", s)  # jie を je に変える (pykakasiはジェをjieにしてしまう)
-    s = re.sub("ch", "t", s)  # ch を t に変える (どちらもテイになる)
-    s = re.sub("ts", "t", s)  # ts を t に変える (どちらもテイになる)
-    s = re.sub("sh", "s", s)  # sh を s に変える (どちらもセイになる)
-    s = re.sub("ji", "di", s)  # ji を di に変える (どちらもデイになる)
-    s = re.sub("fu", "hu", s)  # fu を hu に変える (どちらもヘイになる)
-    s = re.sub(f"({cons})y", "\\1", s)  # 子音 + y を子音に変える
-    s = re.sub(f"([ksthmyrwgzdbpvjf])\\1", "\\1", s)  # n 以外の子音の連続(「っ」)を消す
-    print("前処理 " + s)
 
     # 子音+母音 を 子音 + "ei" に変換
     # (ただし母音が連続する場合は変換しない)
     # s = re.sub(f"({cons}){vowels}({cons})", "\\1ei\\2", s)
     s = re.sub(f"({cons}){vowels}", "\\1ei", s)
     s = re.sub(f"({cons}){vowels}$", "\\1ei", s)
-    print("子音+母音変換 " + s)
+    print("RTOF 子音+母音変換 " + s)
 
     # 母音の連続を"ei"に変換
     ptrn = f"(?:{unit})|(?:{vowels}{vowels})"
     s = re.sub(f"({ptrn}){vowels}{vowels}", "\\1ei", s)
     s = re.sub(f"^{vowels}{vowels}", "ei", s)
-    print("母音連続変換 " + s)
+    print("RTOF 母音連続変換 " + s)
 
     # 孤立した母音を"ei"に変換
     ptrn = f"(?:{unit})|n|'"
     s = re.sub(f"({ptrn}){vowels}({ptrn})", "\\1ei\\2", s)
     s = re.sub(f"({ptrn}){vowels}$", "\\1ei", s)
     s = re.sub(f"^{vowels}({ptrn})", "ei\\1", s)
-    print("孤立母音変換 " + s)
+    print("RTOF 孤立母音変換 " + s)
 
     return s

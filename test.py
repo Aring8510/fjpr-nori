9ee20c01d8c2c918dc4c5159289e9cb2773bbf30
equal-l2 <eng.equall2@gmail.com>
2019-03-16 14:13:10 +0900

    Fix test

--- test.py
+++ test.py
@@ -6,27 +6,27 @@ import ftok
 
 
 test_strs = [
-    ("いいね", "iine",  "einei",  "エイネイ"),
-    ("ダメ",   "dame",  "deimei", "デイメイ"),
-    ("酒",     "sake",  "seikei", "セイケイ"),
-    ("うんち", "u'n'ti", "ei'n'tei", "エインテイ"),
-    ("ほげ", "hoge", "heigei", "ヘイゲイ"),
-    ("どう", "dou", "deiei", "デイエイ"),
-    ("シャツ", "satu", "seitei", "セイテイ"),
-    ("ウーパールーパー", "uparupa", "eipeireipei", "エイペイレイペイ"),
-    ("鼻血", "hanadi", "heineidei", "ヘイネイデイ"),
-    ("フレロビウム", "hurerobiumu", "heireireibeieimei", "ヘイレイレイベイエイメイ"),
-    ("ジェイソン", "jeiso'n'", "jeieisei'n'", "ジェイエイセイン"),
-    ("コンピュータ", "ko'n'puta", "kei'n'peitei", "ケインペイテイ"),
-    ("業務", "goumu", "geieimei", "ゲイエイメイ"),
-    ("般若", "ha'n'na", "hei'n'nei", "ヘインネイ"),
-    ("さっき", "saki", "seikei", "セイケイ"),
-    ("アイアイ", "aiai", "eiei", "エイエイ"),
-    ("本案", "ho'n'a'n'", "hei'n'ei'n'", "ヘインエイン"),
-    ("ウィキペディア", "wikipedia", "weikeipeideiei", "ウェイケイペイデイエイ"),
-    ("ヴァーチャル", "bataru", "beiteirei", "ベイテイレイ"),
-    ("空海さ", "kuukaisa", "keieikeieisei", "ケイエイケイエイセイ"),
-    ("んをい", "'n''o'i", "'n'eiei", "ンエイエイ")
+    ("いいね",           [["iine", True]],        [["einei", True]],             "エイネイ"),
+    ("ダメ",             [["dame", True]],        [["deimei", True]],            "デイメイ"),
+    ("酒",               [["sake", True]],        [["seikei", True]],            "セイケイ"),
+    ("うんち",           [["u'n'ti", True]],      [["ei'n'tei", True]],          "エインテイ"),
+    ("ほげ",             [["hoge", True]],        [["heigei", True]],            "ヘイゲイ"),
+    ("どう",             [["dou", True]],         [["deiei", True]],             "デイエイ"),
+    ("シャツ",           [["satu", True]],        [["seitei", True]],            "セイテイ"),
+    ("ウーパールーパー", [["uparupa", True]],     [["eipeireipei", True]],       "エイペイレイペイ"),
+    ("鼻血",             [["hanadi", True]],      [["heineidei", True]],         "ヘイネイデイ"),
+    ("フレロビウム",     [["hurerobiumu", True]], [["heireireibeieimei", True]], "ヘイレイレイベイエイメイ"),
+    ("ジェイソン",       [["jeiso'n'", True]],    [["jeieisei'n'", True]],       "ジェイエイセイン"),
+    ("コンピュータ",     [["ko'n'puta", True]],   [["kei'n'peitei", True]],      "ケインペイテイ"),
+    ("業務",             [["goumu", True]],       [["geieimei", True]],          "ゲイエイメイ"),
+    ("般若",             [["ha'n'na", True]],     [["hei'n'nei", True]],         "ヘインネイ"),
+    ("さっき",           [["saki", True]],        [["seikei", True]],            "セイケイ"),
+    ("アイアイ",         [["aiai", True]],        [["eiei", True]],              "エイエイ"),
+    ("本案",             [["ho'n'a'n'", True]],   [["hei'n'ei'n'", True]],       "ヘインエイン"),
+    ("ウィキペディア",   [["wikipedia", True]],   [["weikeipeideiei", True]],    "ウェイケイペイデイエイ"),
+    ("ヴァーチャル",     [["bataru", True]],      [["beiteirei", True]],         "ベイテイレイ"),
+    ("空海さ",           [["kuukaisa", True]],    [["keieikeieisei", True]],     "ケイエイケイエイセイ"),
+    ("んをい",           [["'n''o'i", True]],     [["'n'eiei", True]],           "ンエイエイ")
 ]
 
 
@@ -34,17 +34,17 @@ class FJPRTest(unittest.TestCase):
     def test_jtor(self):
         for t in test_strs:
             with self.subTest(src=t[0], dst=t[1]):
-                self.assertMultiLineEqual(jtor.conv_to_roman(t[0]), t[1])
+                self.assertEqual(jtor.conv_to_roman(t[0]), t[1])
 
     def test_rtof(self):
         for t in test_strs:
             with self.subTest(src=t[1], dst=t[2]):
-                self.assertMultiLineEqual(rtof.conv_to_fjpr(t[1]), t[2])
+                self.assertEqual(rtof.conv_to_fjpr(t[1]), t[2])
 
     def test_ftok(self):
         for t in test_strs:
             with self.subTest(src=t[2], dst=t[3]):
-                self.assertMultiLineEqual(ftok.conv_to_kana(t[2]), t[3])
+                self.assertEqual(ftok.conv_to_kana(t[2]), t[3])
 
 
 if __name__ == '__main__':
